d8230e9bb03550615125bda846a5857b46d2b3d5
Christopher Sellers <cjdsellers@protonmail.com>
2017-10-08 20:57:26 +1100

    Add project files.

---
+++ FuzzyLogic/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FuzzyLogic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FuzzyLogic")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("78abd822-85fe-4fcb-a34d-f7ac93668527")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
