821811e51c7c4c0fcb0ce6d9464f281628b9e19b
Christopher Sellers <cjdsellers@protonmail.com>
2017-10-14 11:02:28 +1100

    Adding inference classes.

---
+++ Source/FuzzyLogic/Inference/FuzzyDatabase.cs
@@ -0,0 +1,65 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="FuzzyDatabase.cs" author="Christopher Sellers">
+//   Copyright (C) 2017. All rights reserved.
+//   https://github.com/cjdsellers/FuzzyLogic
+//   the use of this source code is governed by the Apache 2.0 license
+//   as found in the LICENSE.txt file.
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+namespace FuzzyLogic.Inference
+{
+    using System.Collections.Generic;
+
+    using FuzzyLogic.Utility;
+
+    /// <summary>
+    /// The fuzzy database.
+    /// </summary>
+    public class FuzzyDatabase
+    {
+        private readonly Dictionary<string, LinguisticVariable> variables;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FuzzyDatabase"/> class.
+        /// </summary>
+        /// <param name="variables">
+        /// The dictionary of variables.
+        /// </param>
+        public FuzzyDatabase(Dictionary<string, LinguisticVariable> variables)
+        {
+            Validate.NotNull(variables, nameof(variables));
+
+            this.variables = variables;
+        }
+
+        /// <summary>
+        /// Returns the <see cref="LinguisticVariable"/> of the given variable name.
+        /// </summary>
+        /// <param name="variableName">
+        /// The variable name.
+        /// </param>
+        /// <returns>
+        /// The <see cref="LinguisticVariable"/>.
+        /// </returns>
+        public LinguisticVariable GetVariable(string variableName)
+        {
+            return this.variables[variableName];
+        }
+
+        /// <summary>
+        /// Returns a <see cref="bool"/> indicating whether the <see cref="FuzzyDatabase"/>
+        /// contains the variable.
+        /// </summary>
+        /// <param name="variable">
+        /// The variable.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool ContainsVariable(LinguisticVariable variable)
+        {
+            return this.variables.ContainsValue(variable);
+        }
+    }
+}
---
+++ Source/FuzzyLogic/Inference/LinguisticVariable.cs
@@ -0,0 +1,119 @@
+// -------------------------------------------------------------------------------------------------
+// <copyright file="LinguisticVariable.cs" author="Christopher Sellers">
+//   Copyright (C) 2017. All rights reserved.
+//   https://github.com/cjdsellers/FuzzyLogic
+//   the use of this source code is governed by the Apache 2.0 license
+//   as found in the LICENSE.txt file.
+// </copyright>
+// -------------------------------------------------------------------------------------------------
+
+namespace FuzzyLogic.Inference
+{
+    using System.Collections.Generic;
+
+    using FuzzyLogic.Utility;
+
+    /// <summary>
+    /// The linguistic variable immutable class.
+    /// </summary>
+    public class LinguisticVariable
+    {
+        private readonly Dictionary<string, FuzzySet> fuzzySets;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinguisticVariable"/> class.
+        /// </summary>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <param name="lowerBound">
+        /// The lower bound.
+        /// </param>
+        /// <param name="upperBound">
+        /// The upper bound.
+        /// </param>
+        /// <param name="fuzzySets">
+        /// The fuzzy sets.
+        /// </param>
+        public LinguisticVariable(
+            string name,
+            double lowerBound,
+            double upperBound,
+            Dictionary<string, FuzzySet> fuzzySets)
+        {
+            Validate.NotNull(name, nameof(name));
+            Validate.NotNull(fuzzySets, nameof(fuzzySets));
+
+            this.Name = name;
+            this.LowerBound = NonNegativeDouble.Create(lowerBound);
+            this.UpperBound = NonNegativeDouble.Create(upperBound);
+            this.fuzzySets = fuzzySets;
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the lower bound of the <see cref="LinguisticVariable"/>.
+        /// </summary>
+        public NonNegativeDouble LowerBound { get; }
+
+        /// <summary>
+        /// Gets the upper bound of the <see cref="LinguisticVariable"/>.
+        /// </summary>
+        public NonNegativeDouble UpperBound { get; }
+
+        /// <summary>
+        /// The is member.
+        /// </summary>
+        /// <param name="labelName">
+        /// The label name.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool IsMember(string labelName)
+        {
+            Validate.NotNull(labelName, nameof(labelName));
+
+            return this.fuzzySets.ContainsKey(labelName);
+        }
+
+        /// <summary>
+        /// The get set.
+        /// </summary>
+        /// <param name="labelName">
+        /// The set name.
+        /// </param>
+        /// <returns>
+        /// The <see cref="FuzzySet"/>.
+        /// </returns>
+        public FuzzySet GetSet(string labelName)
+        {
+            Validate.NotNull(labelName, nameof(labelName));
+
+            return this.fuzzySets[labelName];
+        }
+
+        /// <summary>
+        /// The get set membership.
+        /// </summary>
+        /// <param name="labelName">
+        /// The set name.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
+        public double GetLabelMembership(string labelName, double value)
+        {
+            Validate.NotNull(labelName, nameof(labelName));
+
+            return this.fuzzySets[labelName].GetMembership(value);
+        }
+    }
+}
