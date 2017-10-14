656d29da7531e1db67cfffe4c61aa932549a94b4
Christopher Sellers <cjdsellers@protonmail.com>
2017-10-14 17:47:24 +1100

    FuzzyLogic.Tests now targeting .NET Framework 4.7 (to run xunit).

---
+++ Source/FuzzyLogic.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FuzzyLogic.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FuzzyLogic.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("30ec247f-f508-416a-860e-020fed5627c2")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
--- Source/FuzzyLogic/Properties/AssemblyInfo.cs
+++ Source/FuzzyLogic/Properties/AssemblyInfo.cs
@@ -1,2 +1,3 @@
 ﻿
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("FuzzyLogic.Tests")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("FuzzyLogic.Tests2")]
