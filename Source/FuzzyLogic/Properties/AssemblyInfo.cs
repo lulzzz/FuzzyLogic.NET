8b6bda7ef45def3f1880cb46536f62d70dc6bc11
Christopher Sellers <cjdsellers@protonmail.com>
2017-10-14 15:36:11 +1100

    Migrated to .NET Standard 2.0.

--- Source/FuzzyLogic/Properties/AssemblyInfo.cs
+++ Source/FuzzyLogic/Properties/AssemblyInfo.cs
@@ -1,18 +1,2 @@
 ﻿
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("FuzzyLogic")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FuzzyLogic")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("78abd822-85fe-4fcb-a34d-f7ac93668527")]
-[assembly: AssemblyVersion("0.0.3.0")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
-[assembly: InternalsVisibleTo("FuzzyLogic.Tests")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("FuzzyLogic.Tests")]
