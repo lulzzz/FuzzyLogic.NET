3317f139d42c4514395c99e4b8b5a996866b6050
Christopher Sellers <cjdsellers@protonmail.com>
2017-10-31 18:21:12 +1100

    Updated PureAttribute remarks.

--- Source/FuzzyLogic/Annotations/PureAttribute.cs
+++ Source/FuzzyLogic/Annotations/PureAttribute.cs
@@ -14,6 +14,14 @@ namespace FuzzyLogic.Annotations
     /// <summary>
     /// This decoration indicates that the annotated method is pure, and thus has no side effects.
     /// </summary>
+    /// <remarks>
+    ///     Properties of a pure function include;
+    ///     - Does not produce side effects (including assigning a variable, or throwing an exception).
+    ///     - Does not mutate the input arguments in any way.
+    ///     - Does not depend on anything external to itself (or reference any global variable).
+    ///     - Produces the same output for a given input.
+    ///     - Is stateless.
+    /// </remarks>
     [AttributeUsage(AttributeTargets.Method)]
     internal sealed class PureAttribute : Attribute
     {
